4ce680aefdc35a519f02667385985d977a93586c
Mikucki <piotr.mikucki@op.pl>
2022-10-20 16:40:10 +0200

    added second page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 // get the background image from the API pls TODO MEGA IMPORTANT (better quality )
+
+fetch("http://www.omdbapi.com/?apikey=[yourkey]&")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
