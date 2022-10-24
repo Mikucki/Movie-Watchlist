5ce6d1b4676f9af3fd1c526d2f89440327b6ecf4
Mikucki <piotr.mikucki@op.pl>
2022-10-24 22:03:22 +0200

    added images and made market search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // get the background image from the API pls TODO MEGA IMPORTANT (better quality )
 
-fetch("http://www.omdbapi.com/?t=ninja&y=2000&plot=full")
+fetch("https://image.unsplash.com/example")
   .then((res) => res.json())
   .then((data) => console.log(data));
