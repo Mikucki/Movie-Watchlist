8a55b073c7242cd866dc89613ef922163a1e40f7
Mikucki <piotr.mikucki@op.pl>
2022-10-24 20:53:56 +0200

    added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // get the background image from the API pls TODO MEGA IMPORTANT (better quality )
 
-fetch("http://www.omdbapi.com/?apikey=[yourkey]&")
+fetch("http://www.omdbapi.com/?t=ninja&y=2000&plot=full")
   .then((res) => res.json())
   .then((data) => console.log(data));
